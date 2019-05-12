d01eeab7e4d0e51fdbd68ed17bf47335d6bd0003
Pedro Lopez <pedroslopez@me.com>
2019-05-12 03:01:45 -0400

    [FIX] Use port from environment variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 const { serveHTTP, publishToCentral } = require("stremio-addon-sdk")
 const addonInterface = require("./src/addon")
-serveHTTP(addonInterface, { port: 55321 })
+serveHTTP(addonInterface, { port: process.env.PORT || 55321 })
 
 // when you've deployed your addon, un-comment this line
 // publishToCentral("https://my-addon.awesome/manifest.json")
